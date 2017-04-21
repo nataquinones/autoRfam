32499fffb06b67ef492fe9d8ffcf26a6611e1ac5
nataquinones <nataquinones@gmail.com>
2017-04-21 15:00:10 +0100

    Remove main function, clean

--- scripts/clean_ali.py
+++ scripts/clean_ali.py
@@ -1,26 +1,45 @@
+"""
+Description:
+Takes .tsv file with sequences marked for deletion (from marktoclean.py) and
+alignments directory. It deletes the specific sequences and makes a copy of
+the alignments in a new directory called "clean_alignments".
+
+Use:
+clean_ali.py <in> <aligmentsdir>
+<in> .tsv file with sequences marked for deletion
+<aligmentsdir> directory with alignments to clean
+"""
+
 import os
 import pandas as pd
 import shutil
 import sys
 
 
-def read_csv(tbl_file):
-    # read tsv table into pandas df
-    df_tbl = pd.read_csv(tbl_file, sep="\t")
+def read_tsv(tsvin):
+    """
+    Read tsv file into pandas df
+    """
+    df_tbl = pd.read_csv(tsvin, sep="\t")
     return df_tbl
 
 
-def cleanup(df_tbl, ali_path, selali_path):
+def cleanup(df_tbl, allali_path):
     """
+    Takes df with sequences marked for deletion, cleans the alignment
+    and makes a copy in directory "clean_alignments/"
     """
+    selali_path = os.path.join(os.path.dirname(
+                               os.path.dirname(allali_path)),
+                               "clean_alignments")
     os.mkdir(selali_path)
     # select unmarked sequences to delete
-    df_todel = df_tbl[df_tbl["selected"] != "*"]
-    # select seq_names to delete lines, in and out file names
-    for urs in set(df_todel["query"]):
-        bad_seqs = df_todel[df_todel["query"] == urs]["seq_name"].tolist()
-        ali_file = ali_path + urs + ".sto"
-        clean_ali_file = selali_path + urs + ".cl.sto"
+    df_todel = df_tbl[df_tbl["keep"] != "*"]
+    # select sequence to delete lines, in and out file names
+    for urs in set(df_todel["alignment"]):
+        bad_seqs = df_todel[df_todel["alignment"] == urs]["sequence"].tolist()
+        ali_file = os.path.join(allali_path, urs + ".sto")
+        clean_ali_file = os.path.join(selali_path, urs + ".cl.sto")
         # process to delete lines with bad seqs
         with open(ali_file) as oldfile, open(clean_ali_file, 'w') as newfile:
             for line in oldfile:
@@ -28,22 +47,17 @@ def cleanup(df_tbl, ali_path, selali_path):
                     newfile.write(line)
 
     # select files that need no modifications to copy
-    to_move = set(df_tbl["query"]) - set(df_todel["query"])
+    to_move = set(df_tbl["alignment"]) - set(df_todel["alignment"])
     for urs in to_move:
-        move_from = ali_path + urs + ".sto"
-        move_to = selali_path + urs + ".sto"
+        move_from = os.path.join(allali_path, urs + ".sto")
+        move_to = os.path.join(selali_path, urs + ".sto")
         shutil.copy(move_from, move_to)
-
-
-def main(tbl_file, ali_path, selali_path):
-        df_tbl = read_csv(tbl_file)
-        cleanup(df_tbl, ali_path, selali_path)
 
 
 # .........................................................................
 
 if __name__ == '__main__':
-    TBL_FILE = sys.argv[1]
-    ALI_PATH = sys.argv[2]
-    SEL_ALI_PATH = sys.argv[3]
-    main(TBL_FILE, ALI_PATH, SEL_ALI_PATH)
+    IN_TSV = sys.argv[1]
+    IN_ALLALIPATH = sys.argv[2]
+    DF_TBL = read_tsv(IN_TSV)
+    cleanup(DF_TBL, IN_ALLALIPATH)
