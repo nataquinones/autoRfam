f9e141cc68882cc3481fee17df65de3524aadbc1
nataquinones <nataquinones@gmail.com>
2017-04-21 14:56:22 +0100

    Integrate into single clean function

--- scripts/get_fasta.py
+++ scripts/get_fasta.py
@@ -1,50 +1,36 @@
 """
 Description:
-Takes a single column .txt file with one non-species specific RNAcentral URS
-per line and write a fasta file with the sequences.
+Uses RNAcentral URSs to fetch sequences in FASTA format from RNAcentral API.
+
+Use:
+get_fasta.py <in> <out>
+<in> input .txt with (non-species specific) RNAcentral URS, one per line
+<out> output .fasta file with sequences
 """
 
 import requests
-import pandas as pd
 import sys
 
 # .............................FUNCTIONS...................................
 
 
-def tabletolist(tsvtbl):
+def fasta_seq(txtfile, fastafile):
     """
-    Takes single column txt file and
-    makes it a python list
+    Fetches FASTA format sequences from txt file of RNAcentral URSs.
+    txtfile: input .txt with RNAcentral URS, one per line
+    fastafile: output .fasta file with sequences
     """
-    urs_df = pd.read_table(
-        tsvtbl,
-        header=None)
-    urs_list = urs_df[0].tolist()
-    return urs_list
-
-
-def fasta_seq(urs_list, outfile):
-    """
-    Fetches FASTA format sequences
-    from a python list of RNAcentral URSs
-    """
-    out_file = open(outfile, "w")
+    urs_list = open(txtfile).read().splitlines()
+    handle = open(fastafile, "w")
     for i in urs_list:
-        url = ("http://rnacentral.org/api/v1/rna/" +
-               i +
-               "?format=fasta")
-        req = requests.get(url, timeout=30)
-        out_file.write(req.content)
-    out_file.close()
-
-
-def main(urs_file, fasta_file):
-    lista = tabletolist(urs_file)
-    fasta_seq(lista, fasta_file)
+        url = ("http://rnacentral.org/api/v1/rna/%s?format=fasta") % i
+        req = requests.get(url, timeout=60)
+        handle.write(req.content)
+    handle.close()
 
 # .........................................................................
 
 if __name__ == '__main__':
-    URS_FILE = sys.argv[1]
-    FASTA_FILE = sys.argv[2]
-    main(URS_FILE, FASTA_FILE)
+    IN_TXT = sys.argv[1]
+    OUT_FASTA = sys.argv[2]
+    fasta_seq(IN_TXT, OUT_FASTA)
