6943e532d1de2238a7988628359b1e94d18aa131
nataquinones <nataquinones@gmail.com>
2017-04-21 15:01:39 +0100

    Remove main function, cleanup

--- scripts/marktoclean.py
+++ scripts/marktoclean.py
@@ -1,10 +1,26 @@
+"""
+Description:
+Takes .tsv file with columns "query" and "target"
+(made for nhmmertbl_parse.py output) and gives a .tsv file of the of the
+sequences to remove from alignments
+
+Use:
+marktoclean.py <in> <out>
+<in> .tsv file with columns "query" and "target"
+<out> .tsv file with sequences marked for deletion
+"""
+
 import sys
 import pandas as pd
 
+# .............................FUNCTIONS...................................
 
-def read_csv(tbl_file):
-    # read tsv table into pandas df
-    df_tbl = pd.read_csv(tbl_file, sep="\t")
+
+def read_tsv(tsvin):
+    """
+    Read tsv file into pandas df
+    """
+    df_tbl = pd.read_csv(tsvin, sep="\t")
     return df_tbl
 
 
@@ -13,45 +29,55 @@ def add_len(df_tbl):
     MODIFIES dataframe f to add "name" column which is made of the
     target name, alifrom and alito. Adds empty "selected" column.
     """
-    seqalen = abs(df_tbl["alifrom"] - df_tbl["alito"])
-    df_tbl["seqalen"] = seqalen
-    df_tbl["seq_name"] = df_tbl["target"] + "/" + df_tbl["alifrom"].map(str) + "-" + df_tbl["alito"].map(str)
+    df_tbl["seqalen"] = abs(df_tbl["alifrom"] - df_tbl["alito"])
+    df_tbl["seq_name"] = df_tbl["target"] + \
+                         "/" + \
+                         df_tbl["alifrom"].map(str) + \
+                         "-" + \
+                         df_tbl["alito"].map(str)
     df_tbl["selected"] = None
     return df_tbl
 
 
-def markbest_perquery(df_tbl, tbl_out):
+def markbest_perquery(df_tbl, report=False):
     """
-    MODIFIES dataframe to mark with * the sequence to keep.
-    -Uncomment lines to make file reporting progress
+    Makes df with * mark on the sequence to keep.
+    If you want to write a file checking the progress of this process
+    add the path as a second argument like:
+    markbest_perquery(df_tbl, "./marktoclean_progress.txt")
     """
-    # m = 1
-    for i in set(df_tbl["query"]):
-        allseqs = df_tbl[df_tbl["query"] == i]["target"]
+    df_bestmark = df_tbl.copy()
+    count = 1
+    for i in set(df_bestmark["query"]):
+        allseqs = df_bestmark[df_bestmark["query"] == i]["target"]
         if len(allseqs) == len(set(allseqs)):
-            df_tbl.set_value((allseqs.index.tolist()), "selected", "*")
+            df_bestmark.set_value((allseqs.index.tolist()), "selected", "*")
         else:
-            for j in set(df_tbl[df_tbl["query"] == i]["target"]):
-                repeats = df_tbl[(df_tbl["query"] == i) & (df_tbl["target"] == j)]
+            for j in set(df_bestmark[df_bestmark["query"] == i]["target"]):
+                repeats = df_bestmark[(df_bestmark["query"] == i) & (df_bestmark["target"] == j)]
                 nonred = repeats.nlargest(1, "seqalen")
-                df_tbl.set_value((nonred.index.tolist()[0]), "selected", "*")
-        # report = "query %d of %d \n" % (m, len(set(df_tbl["query"])))
-        # f = open("./aliclean_prog.txt", 'a')
-        # f.write(out)
-        # f.close()
-        # m = m + 1
-    df_bestmark = df_tbl[["query", "seq_name", "selected"]]
-    df_bestmark.to_csv(tbl_out, sep='\t', index=False)
+                df_bestmark.set_value((nonred.index.tolist()[0]), "selected", "*")
+        if report != False:
+            out = "query %d of %d \n" % (count, len(set(df_bestmark["query"])))
+            handle = open(report, 'a')
+            handle.write(out)
+            handle.close()
+            count = count + 1
+    df_bestmark = df_bestmark[["query", "seq_name", "selected"]]
+    df_bestmark.columns = (["alignment", "sequence", "keep"])
+    return df_bestmark
 
 
-def main(tbl_file, tbl_out):
-        df_tbl = read_csv(tbl_file)
-        df_tbl = add_len(df_tbl)
-        markbest_perquery(df_tbl, tbl_out)
+def main(tsvin):
+        df_tbl = read_tsv(tsvin)
+        add_len(df_tbl)
+        df_bestmark = markbest_perquery(df_tbl)
+        return df_bestmark
 
 # .........................................................................
 
 if __name__ == '__main__':
-    TBL_FILE = sys.argv[1]
-    TBL_OUT = sys.argv[2]
-    main(TBL_FILE, TBL_OUT)
+    IN_TSV = sys.argv[1]
+    OUT_TSV = sys.argv[2]
+    OUT_DF = main(IN_TSV)
+    OUT_DF.to_csv(OUT_TSV, sep='\t', index=False)
