e7823a4aee92b67e90a4c8b9c92a43fad3ac71ad
nataquinones <nataquinones@gmail.com>
2017-05-08 16:36:15 +0100

    Improve code format only

--- scripts/get_fasta.py
+++ scripts/get_fasta.py
@@ -1,12 +1,13 @@
 """
 Description:
-Uses RNAcentral URSs to fetch sequences in FASTA format from RNAcentral API.
+Takes file of RNAcentral URSs, fetches the sequences in .fasta format from
+RNAcentral API. and saves them into file.
 
 Use:
 get_fasta.py <in> <out>
 
-<in> input .txt with (non-species specific) RNAcentral URS, one per line
-<out> output .fasta file with sequences
+<in>: Input list of non species-specific RNAcentral URSs, one per line
+<out>: Output .fasta file
 """
 
 import requests
@@ -18,16 +19,23 @@ import sys
 def fasta_seq(txtfile, fastafile):
     """
     Fetches FASTA format sequences from .txt file of RNAcentral URSs.
-    
+
     txtfile: input .txt with RNAcentral URS, one per line
     fastafile: output .fasta file with sequences
     """
+
+    # read txt file as list
     urs_list = open(txtfile).read().splitlines()
+    # open file to write output
     handle = open(fastafile, "w")
+
+    # fetch each element of the list from RNAcentral API and write file
     for i in urs_list:
         url = ("http://rnacentral.org/api/v1/rna/%s?format=fasta") % i
         req = requests.get(url, timeout=60)
         handle.write(req.content)
+
+    # close file
     handle.close()
 
 # .........................................................................
--- scripts/nhmmer_allvsall.py
+++ scripts/nhmmer_allvsall.py
@@ -1,10 +1,16 @@
 """
-DESCRIPTION:
-Runs NHMMER all vs. all from .fasta file.
-USE:
-nhmmer_allvsall.py <nhmmerpath> <in>
-<nhhmerpath> path to nhmmer software
-<in> input .fasta file
+Description:
+Runs nhmmer with .fasta file against itself with options:
+-o -A --tblout --noali --rna --tformat fasta --qformat fasta
+and returns *.out, *.sto and *.tbl output files.
+
+Use:
+nhmmer_allvsall.py <nhmmerpath> <in> <out_path> <out_name>
+
+<nhmmerpath>: Path to nhmmer from HMMER-3.1b2
+<in>: Input .fasta file
+<out_path>: Path where all the output files will be saved
+<out_name>: Name of the output files
 """
 
 import os
@@ -16,24 +22,22 @@ import sys
 
 def main(nhmmer_path, fastafile, results_path, results_name):
     """
-    Runs NHMMER with fasta file vs. itself, with options:
-    -o: save output file
-    -A: generate alignment of significant hits
-    --tblout: output in tbl format
-    --noali: remove alignments from general output
-    --rna: for RNA sequences
-    --tformat: Target format in FASTA
-    --qformat: Query format in FASTA
-    ..........
+    Runs nhmmer with .fasta file against itself with options:
+    -o -A --tblout --noali --rna --tformat fasta --qformat fasta
+    and returns *.out, *.sto and *.tbl output files.
+
     nhmmer_path: path to nhmmer software
     fasta: fasta file with sequences to be used as query and target
+    results_path: Path where all the output files will be saved
+    results_name: Name of the output files before extension
     """
-    # output dir and files
+
+    # define output dir and output file names
     os.mkdir(results_path)
     out_file = os.path.join(results_path, results_name + ".out")
     sto_file = os.path.join(results_path, results_name + ".sto")
     tbl_file = os.path.join(results_path, results_name + ".tbl")
-    # process
+    # subprocess template
     nhmmer_template = nhmmer_path + " -o %s"\
                                     " -A %s"\
                                     " --tblout %s"\
@@ -48,13 +52,14 @@ def main(nhmmer_path, fastafile, results_path, results_name):
                              tbl_file,
                              fastafile,
                              fastafile)
+    # run subprocess
     subprocess.call(cmd, shell=True)
 
 # .........................................................................
 
 if __name__ == '__main__':
-    NHMMER_PATH = sys.argv[1]
-    FASTA_FILE = sys.argv[2]
-    RESULTS_PATH = sys.argv[3]
-    RESULTS_NAME = sys.argv[4]
-    main(NHMMER_PATH, FASTA_FILE, RESULTS_PATH, RESULTS_NAME)
+    nhmmer_path = sys.argv[1]
+    fastafile = sys.argv[2]
+    results_path = sys.argv[3]
+    results_name = sys.argv[4]
+    main(nhmmer_path, fastafile, results_path, results_name)
