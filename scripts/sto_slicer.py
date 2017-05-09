31fcb72c3b13ac44240630ce18b0f212333ef06d
nataquinones <nataquinones@gmail.com>
2017-05-09 14:25:22 +0100

    Reorder code, make cleaner. Add try/except to work if the alignment name is not in URS format

--- scripts/sto_slicer.py
+++ scripts/sto_slicer.py
@@ -1,12 +1,13 @@
 """
 Description:
-Takes concatenated stockholm file and slices it into individual .sto
-files named after the first sequence in the alignment. Tested on
-NHMMER's -A output.
+Takes a concatenated .sto file and slices it into all the individual
+alignments, names them in based on the first sequence of alignment.
 
 Use:
-sto_slicer.py <in>
-<in> input concatenated .sto file
+sto_slicer.py <in> <out_dir>
+
+<in>: Input concatenated .sto file
+<out_dir>: Directory where all the sliced .sto files will be saved
 """
 
 import os
@@ -18,51 +19,64 @@ import sys
 
 def slice_sto(stofile, dirpath):
     """
-    Takes concatenated stockholm file and slices it
-    into individual .sto files named after the first sequence
-    in the alignment
-    ---
-    stofile: concatenated stockholm file from NHMMER -A
-    out_dir: directory were individual files will be saved
+    Takes a concatenated .sto file and slices it into all the individual
+    alignments, names them in based on the first sequence of alignment.
+
+    stofile: concatenated .sto file
+    out_dir: directory where all the sliced .sto files will be saved
     """
-    # open and read alignment
+
+    # make directory if it doesn't exist already
     if not os.path.exists(dirpath):
         os.mkdir(dirpath, 0777)
+    # open stockholm file and read
     ali = open(stofile)
     alicontent = ali.read()
-    commpatt = re.compile(r"#=GR.*\n")
-    alicontent = commpatt.sub("", alicontent)
-    commpatt2 = re.compile(r"#=GC PP_cons.*\n")
-    alicontent = commpatt2.sub("", alicontent)
-    commpatt3 = re.compile(r"#=GC RF.*\n")
-    alicontent = commpatt3.sub("", alicontent)
-    commpatt5 = re.compile(r"\d\|")
-    alicontent = commpatt5.sub("", alicontent)
-    # pattern to identify end of alignment, split
-    endpatt = re.compile("\/\/\n")
-    filelist = endpatt.split(alicontent)
-    # pattern to get first sequence aka query, list names
-    namepatt = re.compile(r'\n\nURS.{10}')
+
+    # regex patterns:
+    # Lines with #=GR or #=GC
+    grgc_pattern = re.compile(r"#=G[RC].*\n")
+    # Special digit and bar that nhmmer might append, #|
+    digbar_pattern = re.compile(r"\d\|")
+    # End of alignment, //
+    end_pattern = re.compile(r"\/\/\n")
+    # First sequence of each alignment
+    name_pattern = re.compile(r'\n\nURS.{10}')
+
+    # Use regex to clean lines
+    alicontent = grgc_pattern.sub("", alicontent)
+    alicontent = digbar_pattern.sub("", alicontent)
+
+    # Use regex to split into indiv. alignments
+    filelist = end_pattern.split(alicontent)
+    # Remove last element because it splits into " "
+    filelist.pop()
+    # Add stockholm file end, //, at the end of each alignment
+    filelist = [s + "\\\\" for s in filelist]
+
+    # Make 'names' list out of the first sequence of each alignment
     names = []
-    for i in range(0, len(filelist) - 1):
-        eachname = namepatt.findall(filelist[i])
-        names.append(eachname[0])
-    # clean name
-    bad = re.compile("\n\n")
+    for i in range(0, len(filelist)):
+        eachname = name_pattern.findall(filelist[i])
+        try:
+            names.append(eachname[0])
+        # it wont work if the name is not in URS format, use other name
+        except IndexError:
+            names.append("noURS_%s" % i)
+    # clean the name to remove line breaks
+    badnameprefix_pattern = re.compile("\n\n")
     for i in range(0, len(names)):
-        names[i] = bad.sub("", names[i])
-    # add extension
+        names[i] = badnameprefix_pattern.sub("", names[i])
+    # add extension to names
     names = [s + ".sto" for s in names]
+
     # write files
-    for i in range(0, len(filelist) - 1):
-        seqinfofile = open(os.path.join(dirpath, names[i]), 'w')
-        seqinfofile.write(filelist[i])
-        seqinfofile.write("//")
-        seqinfofile.close()
+    for i in range(0, len(filelist)):
+        handle = open(os.path.join(dirpath, names[i]), 'w')
+        handle.write(filelist[i])
+        handle.close()
 
 # .........................................................................
 
 if __name__ == '__main__':
-    STOFILE = sys.argv[1]
-    DIR = sys.argv[2]
-    slice_sto(STOFILE, DIR)
+    slice_sto(sys.argv[1], sys.argv[2])
